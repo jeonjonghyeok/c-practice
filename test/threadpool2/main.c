97c5d9f0997ffb950584ecf24f83b8a50c428508
jeonjonghyeok <tongher1685@gmail.com>
2021-02-19 01:46:37 +0900

    DBSCDEV-55

--- test/threadpool2/main.c
+++ test/threadpool2/main.c
@@ -15,7 +15,7 @@
 
 void masterProcess();
 void socketProcess();
-void createPoolProcess();
+void createPoolThread();
 void *func_work(void *data);
 void *func_socket(void *data);
 void *func_monitor(void *data);
@@ -30,7 +30,7 @@ task_queue_t queue;
 
 struct sockaddr_in serv_addr, cli_addr;
 
-int serv_sock, cli_sock, cli_addr_size, port,status;
+int serv_sock, cli_sock, cli_addr_size, port,status, thread_cnt;
 int close_flag = 0;
 int work_cnt= 0;
 
@@ -59,6 +59,9 @@ void masterProcess()
   pid_t socket_pid, pool_pid, wait_pid;
 
   createQueue(&queue);
+  createSocketThread();
+  createPoolThread();
+  /*
   socket_pid = fork();
   pool_pid = fork();
   if(socket_pid < 0 || pool_pid < 0)
@@ -71,7 +74,7 @@ void masterProcess()
   else if(pool_pid == 0 && socket_pid != 0)
   {
     printf("pool pid= %ld\n",(long)getpid());
-    createPoolProcess();
+    createPoolThread();
   }
   else if(socket_pid > 0)
   {
@@ -104,6 +107,7 @@ void masterProcess()
     }
 
   }
+  */
 }
 void createMonitorThread()
 {
@@ -111,6 +115,7 @@ void createMonitorThread()
   pthread_create(&thread, NULL, func_monitor,(void *)(long)0);
   pthread_detach(thread);
 }
+/*
 void socketProcess()
 {
   serv_sock = socket(PF_INET,SOCK_STREAM,0);
@@ -150,26 +155,75 @@ void socketProcess()
   close(serv_sock);
   close(cli_sock);
 }
+*/
 
-void createPoolProcess()
+void createPoolThread()
 {
   pthread_t thread;
-  int i;
-  
-  pthread_mutex_init(&mutex, NULL);
+  thread_cnt = 0;
 
-  for(i=0;i<POOL_SIZE;i++)
+  pthread_mutex_init(&mutex, NULL);
+  while(thread_cnt < POOL_SIZE)
   {
-    pthread_create(&thread, NULL, func_work,(void *)(long)i);
-    tids[i] = thread;
+    pthread_mutex_lock(&mutex);
+    if(isEmpty(&queue))
+    {
+      pthread_mutex_unlock(&mutex);
+      usleep(100);
+      continue;
+    }
+    pthread_create(&thread, NULL, func_work,(void *)(long)thread_cnt);
     pthread_detach(thread);
   }
-  wait(&status);
-  int a;
-  scanf("%d",a);
+}
+void *func_work(void *data)
+{
+  task_t tmp;
+  char buf[BUF_SIZE];
+  int id = (int)data;
+
+  thread_cnt++;
+  tmp= deQueue(&queue);
+  pthread_mutex_unlock(&mutex);
+  printf("thread[%d] task socket[%d] work start\n",id,tmp.sock_num);
+  read(tmp.sock_num,buf,sizeof(buf));
+  sleep(20);
+  write(tmp.sock_num,buf,sizeof(buf));
+  printf("thread[%d] work done!\n",id);
+  pthread_mutex_lock(&mutex);
+  work_cnt++;
+  pthread_mutex_unlock(&mutex);
+  thread_cnt++;
 }
 
-/*void *func_socket(void *data)
+void createSocketThread()
+{
+  pthread_t thread;
+
+  pthread_create(&thread, NULL, func_socket, (void *)(long)port);
+  pthread_detach(thread);
+
+  serv_sock = socket(PF_INET,SOCK_STREAM,0);
+  memset(&serv_addr,0,sizeof(serv_addr));
+  cli_addr_size = sizeof(cli_addr);
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = port; 
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if(bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr))== -1)
+  {
+    printf("bind error");
+    exit(1);
+  }
+  if(listen(serv_sock, 10)==-1)
+  {
+    printf("listen error");
+    exit(1);
+  }
+}
+
+void *func_socket(void *data)
 {
   task_t task;
   while(close_flag == 0)
@@ -184,34 +238,28 @@ void createPoolProcess()
 
     task.sock_num = cli_sock;
     enQueue(&queue,task);
-    printf("queue[0]: %d\n",queue.task[0].task_num);
   }
   close(serv_sock);
   close(cli_sock);
 }
-*/
+/*
 void *func_work(void *data)
 {
   int id = (int)data;
   task_t tmp;
   char buf[BUF_SIZE];
 
-    printf("work start%d\n",id);
   while(close_flag == 0)
   {
-    printf("while start%d\n",id);
     pthread_mutex_lock(&mutex);
-    printf("threadid = %d queue[0]: %d\n",id,queue.task[0].sock_num);
     if(isEmpty(&queue))
     {
-      printf("empty thread%d\n",id);
       pthread_mutex_unlock(&mutex);
       sleep(10);
       continue;
     }
     else
     {
-      printf("dequeue start%d\n",id);
       tmp= deQueue(&queue);
       pthread_mutex_unlock(&mutex);
       printf("thread[%d] task socket[%d] work start\n",id,tmp.sock_num);
@@ -227,6 +275,7 @@ void *func_work(void *data)
   }
   pthread_exit((void *)0);
 }
+*/
 void *func_monitor(void *data)
 {
   int i,status;
